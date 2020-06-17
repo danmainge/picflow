4c2f4796a0833ccb93cbec3639a2b76195c3263b
danmainge <danmainge@gmail.com>
2020-06-17 12:40:48 +0300

    added constant colors,shadow and padding

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -6,14 +6,22 @@ const textInputDecoration = InputDecoration(
   fillColor: Colors.white,
   filled: true,
   enabledBorder: OutlineInputBorder(
-
       borderSide: BorderSide(
-
     color: Colors.grey,
     width: 1.0,
-
   )),
   focusedBorder: OutlineInputBorder(
     borderSide: BorderSide(color: Colors.grey, width: 2.0),
   ),
 );
+// lists of colors to be used in app
+const kBackgroundColor = Color(0xFF1EFF1);
+const kPrimaryColor = Color(0xFF035AA6);
+const kSecondaryColor = Color(0xFFFFA41B);
+const kTextColor = Color(0xFF000839);
+const kTextLightColor = Color(0xFF747474);
+const kBlueColor = Color(0xFF40BAD5);
+const kDefaultPadding = 20.0;
+// default shadow
+const kDefaultShadow =
+    BoxShadow(offset: Offset(0, 15), blurRadius: 27, color: Colors.black12);
